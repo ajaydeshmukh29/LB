cc1526794fe1a990d7f2e5cd83515d77f8a6a17f
ajaydeshmukh29 <deshmukhajay950@gmail.com>
2026-05-30 05:43:18 +0530

    Dollar to INR

---
+++ Day_7/program31.c
@@ -0,0 +1,29 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Application program which accept amount in US dollar and return its corresponding value in Indian currency. Considering  1$ as 90 rupees
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+int DollarToINR(int iNo)
+{
+  int iINR = 1;
+  int iDollar = 70;
+
+  iINR = iNo * iDollar;
+}
+
+int main()
+{
+  int iValue = 0, iRet = 0;
+
+  printf("Enter number of USD (Input) : ");
+  scanf("%d",&iValue);
+
+  iRet = DollarToINR(iValue);
+
+  printf("Value in INR is (Output) : %d\n",iRet);
+
+  return 0;
+}
