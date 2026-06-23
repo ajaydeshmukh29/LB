6300c6b7a804a11d11efd857129909871f11aad5
ajaydeshmukh29 <deshmukhajay950@gmail.com>
2026-06-24 03:57:31 +0530

    Pattern Printing

---
+++ Day_14/program66.c
@@ -0,0 +1,47 @@
+///////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Accept number or rows and number of columns from user and display below pattern
+//
+//
+//  Input : iRow = 5    iCol = 5
+//
+//  Output : *   *   *   *   *      
+//           *   *   *   *        
+//           *   *   *        
+//           *   *        
+//           *        
+//
+////////////////////////////////////////////////////////////////////////////////////////////
+
+#include<stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+  int i = 0;
+  int j = 0;
+
+  for(i = 1; i <= iRow; i++)
+  {
+    for(j = i; j <= iCol; j++)
+    {
+      printf("*\t");
+    }
+    printf("\n");
+  }
+}
+
+int main()
+{
+  int iValue1 = 0, iValue2 = 0;
+
+  printf("Enter number of Rows : ");
+  scanf("%d",&iValue1);
+
+  printf("Enter number of Columns : ");
+  scanf("%d",&iValue2);
+
+  Pattern(iValue1, iValue2);
+  return 0;
+}
+
+// Time Complexity is : O(N^2)
